c2cd3ba5e87389c0ff7751d204d75f97d758a156
learner-p <andrey.trofim@yandex.ru>
2020-06-17 14:27:01 +0400

    HM les 1

--- homeworks/les1/task5.py
+++ homeworks/les1/task5.py
@@ -7,6 +7,12 @@ a=int(a)
 b=int(b)
 if a>b:
     print(f'Фирма отработала в прибыль {a-b}')
+    while True:
+        c=input('Количество сотрудников: ')
+        if c.isdigit():
+            break
+    c = int(c)
+    print(f'Прибыль из расчета на сотрудника составила {(a-b)/c}')
 elif a<b:
     print('Фирма отработала в убыток')
 else:
