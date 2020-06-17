b2651e6bde15d036c783819022d11e4e97d324df
learner-p <andrey.trofim@yandex.ru>
2020-06-17 13:36:37 +0400

    HM les 1

--- homeworks/les1/task2.py
+++ homeworks/les1/task2.py
@@ -1,7 +1,8 @@
-seconds=input()
-seconds=int(seconds)
-hh=seconds//3600
-mm=(seconds-hh*3600)//60
-ss=seconds-hh*3600-mm*60
-time=str(hh)+':'+str(mm)+':'+str(ss)
-print(time)
+seconds=input('Введите количество секунд\n')
+if seconds.isdigit():
+    seconds=int(seconds)
+    hh=seconds//3600
+    mm=(seconds-hh*3600)//60
+    ss=seconds-hh*3600-mm*60
+    time=f'{hh}:{mm}:{ss}'
+    print(time)
