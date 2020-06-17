e44cb6e0a6e18e2fa86be9ad670c030bde0c52e8
learner-p <andrey.trofim@yandex.ru>
2020-06-17 16:26:08 +0400

    HM les 1

--- homeworks/les1/task4.py
+++ homeworks/les1/task4.py
@@ -1,5 +1,21 @@
-a=input('Ввод числа')
-542
-if a.isdigit():
-    b=9
-    if (a-b)//10
+while True:
+    a = input('Ввод числа: ')
+    if a.isdigit():
+        break
+    print('Некорректный ввод')
+a=int(a)
+
+result=0
+diff=9
+while diff >= 0:
+    b = a
+    while b > 0:
+        if (b-diff)%10==0:
+            result=diff
+            break
+        b=b//10
+    diff -= 1
+    if result>diff:
+        break
+
+print(result)
