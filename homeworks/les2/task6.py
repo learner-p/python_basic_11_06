fbef5e3b4f7eb48e5d0e36647e9cc46996bde281
learner-p <andrey.trofim@yandex.ru>
2020-06-18 21:31:37 +0400

    task6

--- homeworks/les2/task6.py
+++ homeworks/les2/task6.py
@@ -10,5 +10,24 @@ while True:
     if input("Ввод следующего? ")=='нет':
         break
     number += 1
+print('\n')
 for i in result_list:
     print(i)
+
+analityc={}
+name_list=[]
+cost_list=[]
+numbers_list=[]
+metric_list=[]
+for i in result_list:
+    name_list.append(i[1]["Название"])
+    cost_list.append(i[1]["Цена"])
+    numbers_list.append(i[1]["Количество"])
+    metric_list.append(i[1]["Единица измерения"])
+analityc.update({"Название":name_list})
+analityc.update({"Цена":cost_list})
+analityc.update({"Количество":numbers_list})
+analityc.update({"Единица измерения":metric_list})
+
+print('\n')
+print(analityc.items())
