17d844a43e69505d4d0d3185c12c345fdd19252f
learner-p <andrey.trofim@yandex.ru>
2020-07-13 00:07:08 +0400

    HM les8

--- homeworks/les8/task5.py
+++ homeworks/les8/task5.py
@@ -1,30 +1,45 @@
 '''5. Продолжить работу над первым заданием. Разработать методы, отвечающие за приём оргтехники на склад и передачу в определенное
 подразделение компании. Для хранения данных о наименовании и количестве единиц оргтехники, а также других данных, можно использовать
 любую подходящую структуру, например словарь.'''
-
 class Stock:
-    __itemlist=[]
-    def __init__(self,mol,firm):
+    __itemlist={}
+    def __init__(self,mol):
         self.mol=mol
-        self.firm=firm
     @classmethod
-    def get(cls,object):
-        cls.__itemlist.append(object)
+    def get(cls,thing):
+        cls.__itemlist.update({thing:{'наименование':thing.item, 'серийный номер':thing.sn, 'МОЛ':thing.mol}})
+    @classmethod
+    def move_to_mol(cls,obj):
+        cls.__itemlist.pop(obj)
     @classmethod
     def check(cls):
-        for i in cls.__itemlist:
-            print(f'Позиция {i.item}, серийный номер {i.sn}')
+        print(cls.__itemlist)
 class Office:
-    def __init__(self,item,sn):
+    def __init__(self,item,sn,mol):
         self.item=item
         self.sn=sn
+        self.mol=mol
+    def move_to_stock(self):
+        Stock.get(self)
 class Printer(Office):
-    def __init__(self,item,sn):
-        super().__init__(item,sn)
+    def __init__(self,item,sn,mol):
+        super().__init__(item,sn,mol)
 class Scanner(Office):
-    def __init__(self,item,sn):
-        super().__init__(item,sn)
+    def __init__(self,item,sn,mol):
+        super().__init__(item,sn,mol)
 class Xerox(Office):
-    def __init__(self,item,sn):
-        super().__init__(item,sn)
-print(1)
+    def __init__(self,item,sn,mol):
+        super().__init__(item,sn,mol)
+
+a=Printer('hp3230','444334j34534j','hr')
+b=Xerox('lg509','2kljwek23','security')
+c=Scanner('dell','2lkj23lsdl','management')
+if __name__ == '__main__':
+    print(Stock.check())
+    a.move_to_stock()
+    c.move_to_stock()
+    print(Stock.check())
+    b.move_to_stock()
+    print(Stock.check())
+    Stock.move_to_mol(c)
+    print(Stock.check())
