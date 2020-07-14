2528a3aeeb59d2e946da88b4f92fdde7f8efadcc
learner-p <andrey.trofim@yandex.ru>
2020-07-14 17:10:16 +0400

    HM les8

--- homeworks/les8/task1.py
+++ homeworks/les8/task1.py
@@ -2,13 +2,20 @@
 два метода. Первый, с декоратором @classmethod, должен извлекать число, месяц, год и преобразовывать их тип к типу «Число». Второй, с декоратором
 @staticmethod, должен проводить валидацию числа, месяца и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.'''
 class Date:
-    date=input('Ввод даты: ')
+    def __init__(self,date):
+        self.date=date
+        Date.classdate=self.date
     @classmethod
     def number(cls):
-        [day,month,year]=[int(i) for i in cls.date.split('-')]
-        return [day,month,year]
+        day,month,year=[int(i) for i in cls.classdate.split('-')]
+        return day,month,year
     @staticmethod
     def validate():
         return 'ok' if 0<Date.number()[0]<32 else 'not ok', 'ok' if 0<Date.number()[1]<13 else 'not ok'
+
+a=Date('11-07-2020')
+print(Date.number())
+print(Date.validate())
+b=Date('14-07-2020')
 print(Date.number())
 print(Date.validate())
